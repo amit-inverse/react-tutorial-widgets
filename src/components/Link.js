86937553cd372dd465a7658cc37ebe35b81a7733
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-28 18:26:55 +0530

    Handling Command Clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,9 @@
 function Link({ className, to, children }) {
     const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+
         event.preventDefault();
         window.history.pushState({}, '', to);
 
