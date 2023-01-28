b5d1b0a8792958b4a39767c9c29b1f388f2d2efe
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-28 13:14:21 +0530

    Filtering the Option List

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,9 @@
 function Dropdown({ options, selected, onSelectedChange }) {
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+        
         return (
             <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
                 {option.label}
