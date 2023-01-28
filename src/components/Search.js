b4e6ccb1b95048118b7f4b768b2dfcf31110b580
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-28 10:07:27 +0530

    Default Search Terms

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Search() {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    console.log(results);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -15,8 +18,12 @@ function Search() {
                     srsearch: term,
                 },
             });
+
+            setResults(data.query.search);
         };
-        search();
+        if (term) {
+            search();
+        }
     }, [term]);
 
     return (
