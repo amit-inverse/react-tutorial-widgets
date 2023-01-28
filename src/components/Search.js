6f48a72ed6a7694881e3819bee162fe6bbd9a628
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-28 09:23:45 +0530

    Test Input with Hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 function Search() {
-    return <div>Search</div>;
+    const [term, setTerm] = useState('');
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input value={term} onChange={(e) => setTerm(e.target.value)} className="input" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Search;
